889e86a19616c82bac1d28386349df636aed425e
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2020-08-23 17:11:54 +0400

    changed files

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -7,8 +7,8 @@
     " A Mom | A Homemaker | A Cook | A Writer | A Dancer | A Freelancer | PGDM in Finance | Bachelors in Information Technology  ";
 </script>
 
-<style lang="Scss">
-  @import "../ScssStyles/HomeStyle.scss";
+<style type="text/css">
+  @import "../styles/HomeStyle.css";
 </style>
 
 <svelte:head>
